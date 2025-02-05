e600f56f5dd7c2ebf63771c720ea409f2f21de8c
xjl <justdowhatiwant@vip.qq.com>
2025-02-06 02:31:51 +0800

    实现循环下载功能，防休眠与循环下载指令碰撞问题待解决

--- pages/download/download.vue
+++ pages/download/download.vue
@@ -9,7 +9,7 @@
 		</view>
 		<view class="" style="margin-top: 20px;">
 			<text style="font-size: 20px;">Download Path: </text>
-			<button type="default" @click="getHistory">test</button>
+			<button type="default" @click="getData()">test</button>
 			<input type="text" :value="path" style="height: 30px; border: 1px solid grey; background: lightgray; " disabled />
 		</view>
 		<view class="dw_btn">
@@ -48,7 +48,7 @@
 			onShow() {
 				console.log(plus.runtime.arguments);
 			},
-			getCurrent(){
+			getCurrent() {
 				bleInfo.ble_recv_data = "";
 				this.current = 0;
 				let cmdStr = "01 03 00 00 00 00 01";
@@ -76,38 +76,120 @@
 					}
 				});
 			},
+			writeBLECharacteristicValueAsync({
+				deviceId,
+				serviceId,
+				characteristicId,
+				value
+			}) {
+				return new Promise((resolve, reject) => {
+					uni.writeBLECharacteristicValue({
+						deviceId,
+						serviceId,
+						characteristicId,
+						value,
+						success: (res) => {
+							uni.$once("dataArrive", () => {
+								if (crcCheck(bleInfo.ble_recv_data)) {
+									let data = bleInfo.ble_recv_data.slice(8, bleInfo.ble_recv_data.length - 6);
+									resolve(data); // 成功时返回结果
+								}
+							});
+
+						},
+						fail: (err) => {
+							reject(err); // 失败时抛出错误
+						},
+					});
+				});
+			},
+			async getData(modbusCmd) {
+				try {
+					const richAlert = uni.requireNativePlugin("DCloud-FilePicker");
+					this.getCurrent();
+					uni.$once("currentReady", async () => {
+						if (bleInfo.ble_device.name.includes("DWL4")) {
+							let csvData =
+								`Model,DWL-TILT-CDSK,
+SN,${bleInfo.ble_device.name.slice(5,)},,AppVersion,V1.0.0,
+Logging Interval,00hr00min00sec,
+Download Time,2025/02/06 01:48,
+Battery Voltage(V),11.78,
+Number Of Records,${this.current},
+Date/Time,RECORD,Battery Voltage(V),Reading(R1),Reading(R2),Reading(R3),Reading(R4),Reading(R5),Reading(R6),Reading(R7),Reading(R8),Reading(R9),Reading(R10),Reading(R11),
+,,V,Hz,Deg C,Hz,Deg C,Hz,Deg C,Hz,Deg C,Deg C,%RH,hPa`
+							for (var index = 0; index < this.current; index++) {
+								bleInfo.ble_recv_data = "";
+								let cmdStr = `01 03 ${(0x000200+index).toString(16).padStart(6, '0')} 00 01`;
+								// console.log(index, '---', cmdStr);
+								let modbusCmd = getModbusCmdBuf(cmdStr);
+								let data = await this.writeBLECharacteristicValueAsync({
+									deviceId: bleInfo.ble_device.deviceId,
+									serviceId: bleInfo.ble_service.uuid,
+									characteristicId: bleInfo.ble_send_characteristic.uuid,
+									value: modbusCmd
+								});
+								console.log("++++++", data);
+								let datetimeArr = data.slice(0, 12).match(/.{1,6}/g);
+								let collectionDataArr = data.slice(12, ).match(/.{1,8}/g);
+								// let rowData = [...datetime, ...collectionData]
+								// rowData[0] = '20' + datetimeArr[0].match(/.{1,2}/g).join('-');
+								// rowData[1] = datetimeArr[1].match(/.{1,2}/g).join(':');
+								let datetime = '20' + datetimeArr[0].match(/.{1,2}/g).join('-') + ' ' + datetimeArr[1].match(/.{1,2}/g).join(':');
+								let rowData = `${datetime},${index},${collectionDataArr.join(',')}`
+								console.log(index, '---', rowData);
+								csvData += '\r\n' + rowData;
+							}
+							richAlert.saveFile({
+								folder: this.path,
+								fileName: "111.csv",
+								data: csvData
+							}, result => {
+								console.log("======", result);
+							});
+						} else if (bleInfo.ble_device.name.includes("TILT")) {
+							let cmdStr = "01 03 00 00 00 00 15";
+							let modbusCmd = getModbusCmdBuf(cmdStr);
+						}
+					});
+				} catch (error) {
+					//TODO handle the exception
+					console.log("读取数据出错: ", error);
+				}
+			},
 			getHistoryOnce() {
+				this.getCurrent();
 				uni.$once("currentReady", () => {
 					if (bleInfo.ble_device.name.includes("DWL4")) {
 						/*for (let index = 0; index < this.current; index++) {
 							cmdStr = `01 03 ${(0x000200+index).toString(16).padStart(6,'0')} 00 01`;
 							console.log(`+++++${index}+++++`, cmdStr);*/
-							console.log("readDWL4");
-							let cmdStr = "01 03 00 02 00 00 01"
-							let modbusCmd = getModbusCmdBuf(cmdStr);
-							uni.writeBLECharacteristicValue({
-								deviceId: bleInfo.ble_device.deviceId,
-								serviceId: bleInfo.ble_service.uuid,
-								characteristicId: bleInfo.ble_send_characteristic.uuid,
-								value: modbusCmd,
-								success: (res) => {
-									console.log("success 1");
-									uni.$once("dataArrive", () => {
-										console.log("success 2");
-										if (crcCheck(bleInfo.ble_recv_data)) {
-											console.log("success 3");
-											let data = bleInfo.ble_recv_data.slice(8, bleInfo.ble_recv_data.length - 6);
-											console.log('********', data);
-											let datetime = data.slice(0, 12).match(/.{1,6}/g);
-											let collectionData = data.slice(12, ).match(/.{1,8}/g);
-											console.log('------', datetime, collectionData);
-										}
-									});
-								},
-								fail: (res) => {
-									console.log(`读取缓存失败，地址:${(0x000200+index).toString(16).padStart(6,'0')}`);
-								}
-							});
+						console.log("readDWL4");
+						let cmdStr = "01 03 00 02 00 00 01"
+						let modbusCmd = getModbusCmdBuf(cmdStr);
+						uni.writeBLECharacteristicValue({
+							deviceId: bleInfo.ble_device.deviceId,
+							serviceId: bleInfo.ble_service.uuid,
+							characteristicId: bleInfo.ble_send_characteristic.uuid,
+							value: modbusCmd,
+							success: (res) => {
+								console.log("success 1");
+								uni.$once("dataArrive", () => {
+									console.log("success 2");
+									if (crcCheck(bleInfo.ble_recv_data)) {
+										console.log("success 3");
+										let data = bleInfo.ble_recv_data.slice(8, bleInfo.ble_recv_data.length - 6);
+										console.log('********', data);
+										let datetime = data.slice(0, 12).match(/.{1,6}/g);
+										let collectionData = data.slice(12, ).match(/.{1,8}/g);
+										console.log('------', datetime, collectionData);
+									}
+								});
+							},
+							fail: (res) => {
+								console.log(`读取缓存失败，地址:${(0x000200+index).toString(16).padStart(6,'0')}`);
+							}
+						});
 						// }
 					} else if (bleInfo.ble_device.name.includes("TILT")) {
 						let cmdStr = "01 03 00 00 00 00 15";
@@ -129,7 +211,7 @@
 
 		},
 		onShow() {
-			if(bleInfo.ble_connected){
+			if (bleInfo.ble_connected) {
 				this.getCurrent();
 			}
 		}
